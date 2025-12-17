7d262158f38585991b4286016adb31f3c8347bd4
AlexDreemurr <stanleyatcheson187@gmail.com>
2025-12-17 16:59:38 +0800

    日记更新

--- js/clickDiary.js
+++ js/clickDiary.js
@@ -1,3 +1,11 @@
 function clickDiary(obj) {
-    window.location.href = 'diaries/diary1.html';
+    switch (obj.id) {
+        case 'diary-1':    
+            window.location.href = 'diaries/diary1.html';
+            break;
+        case 'diary-2':
+            window.location.href = 'diaries/diary2.html';
+            break;
+    }
+
 }
